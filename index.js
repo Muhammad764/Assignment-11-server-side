de68ec0b48f0b6bdc68d52e6b1c583cf619a59a2
Muhammad764 <farhadislam745@gmail.com>
2022-05-11 19:35:19 +0600

    show data on clint side

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 // middleware
 app.use (cors())
-app.use (express.json())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gknk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('bicycle').collection('items')
+
+        app.get('/item', async (req, res) => {
+            const query={}
+            const cursor = itemsCollection.find(query)
+            const items = await cursor.toArray()
+            res.send(items)
+        })
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const items = await itemsCollection.findOne(query)
+            res.send(items)
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('i hope the server running')
