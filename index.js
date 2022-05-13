2b3da4a4a5cfe2765125c974a418b22811c8b633
Muhammad764 <farhadislam745@gmail.com>
2022-05-13 21:28:36 +0600

    ses

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ async function run() {
         //Delete
         app.delete('/item/:id', async (req, res) => {
             const id = req.params.id
+            console.log(id)
             const query = {_id: ObjectId(id)}
             const result = await itemsCollection.deleteOne(query)
             res.send(result)
